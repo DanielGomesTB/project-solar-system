daa95b4682e12e5a03d9b7b6b723ecf69ea9a754
Daniel Gomes <danielgomesveiga@hotmail.com>
2022-06-21 16:41:56 -0300

    Todos os Requisitos

---
+++ src/components/Missions.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionsCard from './MissionCard';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions.map((mission) => (
+          <MissionsCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.image }
+            country={ mission.country }
+            destination={ mission.destination }
+          />))}
+      </div>
+    );
+  }
+}
+
+export default Missions;
